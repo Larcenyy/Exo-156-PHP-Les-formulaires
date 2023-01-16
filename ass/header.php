ea88255fb15c50ef05ea61cdf6436ecabbb6cd9f
Rémy <remyc59440@hotmail.com>
2023-01-16 13:33:46 +0100

    okk

---
+++ ass/header.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Mon Exo 156</title>
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    require "ass/footer.php";
+    require "ass/header.php";
+?>
+
+
+<form method="post" action="ass/form.php">
+    <label for="username">Nom d'utilisateur</label>
+    <input type="text" id="username" name="name">
+    <input type="submit">
+</form>
+
+
